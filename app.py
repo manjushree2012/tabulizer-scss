a42b1f3b0b0adde30400e62ff7a58299336256ea
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-12-03 14:11:12 +0545

    refactor: moved routeto new file

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-import os
-from flask import Flask, request, jsonify
-from tasks import long_running_task
+from flask import Flask
+from domains.file.routes.routes import file_bp
 
 app = Flask(__name__)
 app.config['broker_url'] = 'redis://localhost:6379/0'
 app.config['result_backend'] = 'redis://localhost:6379/0'
 
+app.register_blueprint(file_bp, url_prefix = '/api')
+
 UPLOAD_DIR = 'uploads'
 PROCESSED_DIR = 'processed'
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    file_path = os.path.join(UPLOAD_DIR, file.filename)
-    file.save(file_path)
-
-    task = long_running_task.delay(file.filename)
-
-    response = {'state': 12, 'error': 'asas'}
-    return jsonify(response, 202)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- domains/file/routes/routes.py
+++ domains/file/routes/routes.py
@@ -1,15 +1,18 @@
 import os
 from flask import Blueprint, request, jsonify
+from tasks import long_running_task
 
 file_bp = Blueprint('file', __name__)
 
 UPLOAD_DIR = 'uploads'
 
 @file_bp.route('/upload', methods=['POST'])
-def create_notes():
+def upload_file():
     file = request.files['file']
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     file.save(file_path)
 
+    task = long_running_task.delay(file.filename)
+
     response = {'state': 12, 'error': 'asas'}
-    return jsonify(response)
+    return jsonify(response, 202)
