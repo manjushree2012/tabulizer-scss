98f336c6b1c46caab5e716b4e42ee7c42cbd1dd5
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2024-12-04 13:58:31 +0545

    log exceptions when parse fail

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import os
 import tabula
 import pandas as pd
 from celery import Celery
+import logging
 
 celery = Celery('tasks', broker='redis://localhost:6379/0')
 
@@ -31,6 +32,16 @@ def parse_pdf(self,filename):
             'path': output_path
         }
     except Exception as exc:
+        #Create a logger
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.ERROR)
+        handler = logging.FileHandler('something.log')
+        handler.setLevel(logging.ERROR)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.error(f"An error occurred in task {task_id}: {str(exc)}")
+
         print(f"An error occurred: {exc}")  # Log the error message
         return {
             'status': 'Task failed',
